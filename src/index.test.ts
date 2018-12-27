86c7b633274a24894361a39e0981ec5782289b40
Jesse Luoto <jesse.luoto@gmail.com>
2018-12-28 00:22:18 +0200

    Add zip function

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,7 +9,8 @@ import {
   scan,
   closer,
   take,
-  fromArray
+  fromArray,
+  zip
 } from './index'
 
 const testIterator = (num: number) => fromArray(Array(num).keys())
@@ -251,5 +252,53 @@ describe('fromArray', () => {
   it('should work with array.keys iterable', async () => {
     const result = await first(collect(fromArray(Array(3).keys())))
     expect(result).toEqual([0, 1, 2])
+  })
+})
+
+describe('zip', () => {
+  it('pairwise combines values from two streams using given combinator function', async () => {
+    const s1 = fromArray([1, 2, 3])
+    const s2 = fromArray(['a', 'b', 'c'])
+    const result = await first(collect(zip(s1, s2, (x, y) => x + y)))
+
+    expect(result).toEqual(['1a', '2b', '3c'])
+  })
+
+  it('should zip to array if no combinator is given', async () => {
+    const s1 = fromArray([1, 2, 3])
+    const s2 = fromArray(['a', 'b', 'c'])
+    const result = await first(collect(zip(s1, s2)))
+
+    expect(result).toEqual([[1, 'a'], [2, 'b'], [3, 'c']])
+  })
+
+  it('completes as soon as possible', async () => {
+    const s1 = fromArray([1])
+    const s2 = fromArray(['a', 'b', 'c'])
+    const result = await first(collect(zip(s1, s2)))
+
+    expect(result).toEqual([[1, 'a']])
+  })
+
+  it('works with endless left stream', async () => {
+    async function* forever() {
+      while (true) {
+        yield true
+      }
+    }
+
+    const result = await first(collect(zip(forever(), fromArray([1, 2, 3]))))
+    expect(result).toEqual([[true, 1], [true, 2], [true, 3]])
+  })
+
+  it('works with endless right stream', async () => {
+    async function* forever() {
+      while (true) {
+        yield 'foo'
+      }
+    }
+
+    const result = await first(collect(zip(fromArray([1, 2, 3]), forever())))
+    expect(result).toEqual([[1, 'foo'], [2, 'foo'], [3, 'foo']])
   })
 })
